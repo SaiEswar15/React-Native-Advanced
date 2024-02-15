8bd04869acb237718113af8ec8b7796535bbfd6b
SaiEswar15 <saieswarkumar@gmail.com>
2024-02-15 23:44:34 +0530

    updating the main <Currency> component

--- projects/currency/currency.tsx
+++ projects/currency/currency.tsx
@@ -1,11 +1,14 @@
 import {StyleSheet, Text, View, SafeAreaView} from 'react-native';
 import React from 'react';
+import CurrencyList from './components/CurrencyList';
+
 
 export default function currency() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.mainContainer}>
         <Text>currency</Text>
+        <CurrencyList></CurrencyList>
       </View>
     </SafeAreaView>
   );
