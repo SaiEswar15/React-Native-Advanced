42a8c3f6484e724b04197a758b6223489b332ff7
SaiEswar15 <saieswarkumar@gmail.com>
2024-02-15 23:43:28 +0530

    currency list component which map all currency Buttons

---
+++ projects/currency/components/CurrencyList.tsx
@@ -0,0 +1,24 @@
+import 
+{ 
+    StyleSheet, 
+    Text, 
+    View 
+} 
+from 'react-native'
+import React from 'react'
+import { currencyByRupee } from '../constants'
+import CurrencyButtons from './CurrencyButtons'
+
+export default function CurrencyList() {
+  return (
+    <View>
+      <Text>Currency List</Text>
+      {currencyByRupee.map((el, index) : JSX.Element=>{
+        return <CurrencyButtons key = {index} name = {el.name} flag = {el.flag} value={el.value} symbol = {el.symbol}></CurrencyButtons>
+      })}
+      
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
